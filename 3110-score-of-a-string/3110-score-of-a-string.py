535ef036f9a2fb58b2a1ea28bac2b43eb8519c54
Tanvi Varshney <tanvi.varshney7@gmail.com>
2026-06-01 16:09:27 -0400

    Time: 0 ms (100%), Space: 12.4 MB (15.77%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        score = 0
+        for i in range(len(s)-1):
+            diff = abs(ord(s[i]) - ord(s[i+1]))
+            score += diff
+        return score
+        
