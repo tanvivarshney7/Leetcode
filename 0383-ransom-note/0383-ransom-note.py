95cd5831b57e6bada3f18b7a985ddefb67f93722
Tanvi Varshney <tanvi.varshney7@gmail.com>
2026-06-19 19:39:36 -0400

    Time: 107 ms (5.02%), Space: 12.7 MB (20.35%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        for char in ransomNote:
+            if ransomNote.count(char) > magazine.count(char):
+                return False
+        return True
