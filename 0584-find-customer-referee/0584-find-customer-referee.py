9b1668eb518e7236115efd5112410d946bcff4c0
Tanvi Varshney <tanvi.varshney7@gmail.com>
2026-07-25 08:53:17 -0400

    Time: 280 ms (75.52%), Space: 67 MB (75.46%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    return customer[
+        (customer["referee_id"] != 2) |
+        (customer["referee_id"].isna())
+    ][["name"]]
+    
